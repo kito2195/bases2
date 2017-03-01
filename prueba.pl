ddcb4f429e0db0adda616514bfec3a00827bd967
kito2195 <kito2195@gmail.com>
2017-02-28 20:36:51 -0600

    Update prueba.pl

--- prueba.pl
+++ prueba.pl
@@ -5,13 +5,15 @@ use CGI::Carp qw(fatalsToBrowser);
 use LWP::UserAgent;
 use HTTP::Request;
 
-my $query_mailto = ("mfrom" => '@gmail.com');
-my $query_mailfrom = ('mfrom' => '@gmail.com');
-my $query_mail = ('q' => '@gmail.com');
+#campos a sustituir en la pagina web
+my $query_mailto = '';
+my $query_mailfrom = '';
+my $query_mail = ''; 
 
 my $tipo_campo = $ARGV[0];
 my $texto = $ARGV[1];
 
+#conexión con la página web
 my $ua = LWP::UserAgent->new;
 $ua->agent("Mozilla/4.0 (compatible; MSIE 5.0; Windows 98; DigExt)");
 
